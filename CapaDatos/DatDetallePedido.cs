452c0ccffc04c65f3d51e30168192e3055dd03bf
JordanRondon <jordanangelrondonpozo@gmail.com>
2023-04-26 10:14:27 -0500

    se agrego las clases DatPedido y DatDetallePedido con su estructura basica

---
+++ CapaDatos/DatDetallePedido.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class DatDetallePedido
+    {
+        #region Patron Sigleton
+        //Variable estática para la instancia
+        //privado para evitar la instanciación directa
+        private static readonly DatDetallePedido _instancia = new DatDetallePedido();
+        public static DatDetallePedido Instancia
+        {
+            get
+            {
+                return DatDetallePedido._instancia;
+            }
+        }
+        #endregion Patron Sigleton
+
+        #region Metodos
+
+        #endregion metodos
+    }
+}
