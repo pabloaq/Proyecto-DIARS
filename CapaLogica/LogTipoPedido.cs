4cf90481e63ec525c2528244e71923e78066d902
ELVIS PAZ <Ewpc240@gmail.com>
2023-04-27 01:23:58 -0500

    Tipo del pedido

---
+++ CapaLogica/LogTipoPedido.cs
@@ -0,0 +1,39 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+namespace CapaLogica
+{
+    public class LogTipoPedido
+    {
+        #region Patron Sigleton
+        private static readonly LogTipoPedido _instancia = new LogTipoPedido();
+        public static LogTipoPedido Instancia
+        {
+            get
+            {
+                return LogTipoPedido._instancia;
+            }
+        }
+        #endregion Patron Sigleton
+
+        #region Metodos
+
+        public List<EntTipoPedido> ListarTipoPedido()
+        {
+            try
+            {
+                List<EntTipoPedido> lista = datTipoPedido.Instancia.ListarTipoPedido();
+                return lista;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+            #endregion Metodos
+        }
+}
---
+++ CapaPresentacion/FormTipoPedido.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaEntidad;
+using CapaLogica;
+namespace CapaPresentacion
+{
+    public partial class FormTipoPedido : Form
+    {
+        public FormTipoPedido()
+        {
+            InitializeComponent();
+            ListarTipoPedido();
+        }
+        public void ListarTipoPedido()
+        {
+            dgvTipoPedido.DataSource = LogTipoPedido.Instancia.ListarTipoPedido();
+        }
+
+
+        private void btnEliminarTipoPedido_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvTipoPedido_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtIDTipoPedido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombreTipoPedido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregarTipoPedido_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModificarTipoPedido_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void lblTipoPedido_Click(object sender, EventArgs e)
+        {
+            /**/
+        }
+
+        private void FormTipoPedido_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
