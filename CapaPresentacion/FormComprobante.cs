a10c7509b6fdf5a7f53874b22e527dad6ce4fab7
JordanRondon <jordanangelrondonpozo@gmail.com>
2023-05-24 12:09:12 -0500

    mostrado del label precio automatico segun pedidoy rediseño del comprobante

--- CapaPresentacion/FormComprobante.cs
+++ CapaPresentacion/FormComprobante.cs
@@ -22,6 +22,9 @@ namespace CapaPresentacion
             InitializeComponent();
             listarCmbPedido();
             listarCmbMetodoPago();
+            MostrarDatosAdicionales();
+            cbxIdPedido.DropDownStyle = ComboBoxStyle.DropDownList;//Evita que el usuario pueda modificar el cuadro de texto
+            cbxMetodoPago.DropDownStyle = ComboBoxStyle.DropDownList;//Evita que el usuario pueda modificar el cuadro de texto
         }
 
         private void listarCmbPedido()
@@ -60,6 +63,11 @@ namespace CapaPresentacion
             return null;
         }
 
+        private void MostrarDatosAdicionales()
+        {
+            lbFechaDePago.Text = DateTime.Today.ToString();
+        }
+
         private void btnRealizarComprobante_Click(object sender, EventArgs e)
         {
             try
@@ -90,6 +98,18 @@ namespace CapaPresentacion
             {
                 MessageBox.Show("Error al generar el comprobante" + ex);
             }
+        }
+
+        private void cbxIdPedido_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            EntComprobante comprobante = new EntComprobante();
+            comprobante.PedidoID = buscarPedidoPorNombreCliente(cbxIdPedido.Text).idPedido;
+            lbMontoTotal.Text = LogComprobante.Instancia.VerificarMontoTotal(comprobante).ToString();
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -14,7 +14,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLogin());
+            Application.Run(new FormComprobante());
 
 
 
