d33163057ecf0bf01aff527ffb215c7d0595ded2
KevinGM02 <kgarcia_m@hotmail.com>
2023-05-26 21:12:58 -0500

    se cambio la conexion local a un host en linea

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -8,6 +8,8 @@ namespace CapaDatos
 
         public static Conexion GetInstancia => instancia;
 
-        public SqlConnection Conectar => new SqlConnection("Data Source=.;Initial Catalog=BDWILSON;" + "Integrated Security=true;");
+        //public SqlConnection Conectar => new SqlConnection("Data Source=.;Initial Catalog=BDWILSON;" + "Integrated Security=true;");
+        public SqlConnection Conectar => new SqlConnection("Data Source = BDWILSON.mssql.somee.com; Initial Catalog = BDWILSON; Integrated Security=False; User Id= kevingm_SQLLogin_1; Password=sgb6jh3iyz"
+           );
     }
 }
--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -14,7 +14,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new formAdministrador());
+            Application.Run(new FormLogin());
         }
     }
 }
